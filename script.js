0f5dea8fcd275133c5e22495cf350d673f39fcf3
ermmme <185312182+ermmme@users.noreply.github.com>
2024-11-13 22:08:35 -0800

    added link element to html, an event listener that prevents it from navigating to the site when clicked, and added an event listener for the link.

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
 const button = document.getElementById('clickOnceButton');
+const link = document.getElementById('favoriteLink');
 
 button.addEventListener('click', function handleClick() {
-    
+
     alert('the button was clicked!!! holy smokes!!');
 
     button.removeEventListener('click', handleClick);
+});
+
+link.addEventListener('click', function(event) {
+    event.preventDefault();
+    alert("No distractions! I’m coding!");
 });
